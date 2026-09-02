d694f46077dab53d3d6f01d155a314a4cf9dce76
chanwhee0330 <chanwhee38@gmail.com>
2026-09-03 00:53:23 +0900

    워밍업2끝

--- Project1/Project1/warmmingup2.c
+++ Project1/Project1/warmmingup2.c
@@ -4,9 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 
 
 char lines[10][100];
+char temp[100];
 
 void openFile()
 {
@@ -39,7 +41,7 @@ int main()
 {
 	openFile();
 	char command;
-	bool isd = false;
+	bool isd = false,isf=false,ish=false;
 	print();
 	while (1)
 	{
@@ -137,12 +139,153 @@ int main()
 				for (int j = 0;lines[i][j] != '\0';j++)
 				{
 					if (lines[i][j] == ' ')
-						lines[i][j]='*';
+						lines[i][j] = '*';
 					else if (lines[i][j] == '*')
 						lines[i][j] = ' ';
 				}
 			}
 			print();
 		}
+		else if (command == 'f')
+		{
+			if (isf == false)
+			{
+				for (int i = 0;i < 10;i++)
+				{
+					for (int j = 0;lines[i][j] != '\0'; j++)
+					{
+						int count = j;
+						while (lines[i][j] != ' ' && lines[i][j] != '*' && lines[i][j] != '\0')
+							j++;
+
+						for (int k = j - 1;k >= count;k--)
+							printf("%c", lines[i][k]);
+
+						if (lines[i][j] == ' ')
+							printf(" ");
+					}
+					printf("\n");
+				}
+				isf = true;
+			}
+			else
+			{
+				print();
+				isf = true;
+			}
+		}
+		else if (command == 'g')
+		{
+			char change,word;
+			scanf(" %c %c", &change,&word);
+			for (int i = 0;i < 10;i++)
+			{
+				for (int j = 0;lines[i][j] != '\0';j++)
+				{
+					if (lines[i][j] == change)
+						lines[i][j] = word;
+				}
+			}
+			print();
+		}
+		else if (command == 'h')
+		{
+			if (ish == false)
+			{
+				for (int i = 0;i < 10;i++)
+				{
+					for (int j = 0;lines[i][j] != '\0';j++)
+					{
+						if (lines[i][j] >= '0' && lines[i][j] <= '9')
+							printf("%c\n", lines[i][j]);
+						else
+							printf("%c", lines[i][j]);
+					}
+					printf("\n");
+				}
+				ish = true;
+			}
+			else
+			{
+				ish = false;
+				print();
+			}
+			
+		}
+		else if (command == 'i')
+		{
+			char word[100];
+			int count = 0;
+
+			scanf(" %s", word);
+
+			for (int i = 0; i < 10; i++)
+			{
+				int start = 0;
+
+				for (int j = 0; ; j++)
+				{
+					if (lines[i][j] == ' ' || lines[i][j] == '\0')
+					{
+						int len = j - start;
+						int same = 1;
+
+						if (strlen(word) != len)
+						{
+							same = 0;
+						}
+						else
+						{
+							for (int k = 0; k < len; k++)
+							{
+								if (tolower(lines[i][start + k]) != tolower(word[k]))
+								{
+									same = 0;
+									break;
+								}
+							}
+						}
+
+						if (same == 1)
+						{
+							count++;
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+						}
+						else
+						{
+							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+						}
+
+						for (int k = start; k < j; k++)
+							printf("%c", lines[i][k]);
+
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+						if (lines[i][j] == ' ')
+							printf(" ");
+
+						if (lines[i][j] == '\0')
+							break;
+
+						start = j + 1;
+					}
+				}
+
+				printf("\n");
+			}
+
+			printf("%s는 %d개입니다.\n", word, count);
+		}
+		else if (command == 'j')
+		{
+			strcpy(temp, lines[9]);
+			for (int i = 8;i >= 0;i--)
+			{
+				strcpy(lines[i + 1], lines[i]);
+			}
+			strcpy(lines[0], temp);
+			print();
+		}
+		else if (command=='q')
+			break;
 	}
 }
